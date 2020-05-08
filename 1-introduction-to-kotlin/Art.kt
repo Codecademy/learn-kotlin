ad0f6fd0e370f75db8ddeb9929572fe77c2f7a56
adistasicc <59616028+adistasicc@users.noreply.github.com>
2020-05-08 11:57:30 -0400

    Create Art.kt
    Add Art.kt

---
+++ 1-introduction-to-kotlin/Art.kt
@@ -0,0 +1,30 @@
+// Project: ASCII Art
+// Author: Alex DiStasi
+
+/* Use a multiline comment to draft the image
+     .....
+  .         .
+ .   0  0    .
+  ...........
+   .. __ ..
+  . .|C_|. .
+ .  ......  .
+     .  .
+     .  .
+*/
+
+fun main() {
+    // Use print statements to output image
+    println("     .....")
+    println("  .         .")
+    println(" .   0  0    .")
+    println("  ...........")
+    println("   .. __ ..")
+    println("  . .|C_|. .")
+    println(" .  ......  .")
+    println("     .  .")
+    println("     .  .")
+
+    // Use a single print statement to output image
+    println("     .....\n  .         .\n .   0  0    .\n  ...........\n   .. __ ..\n  . .|C_|. .\n .  ......  .\n     .  .\n     .  .  ")
+}
