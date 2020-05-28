533842951bb53a0a99d40dfb9c2fc5b117c00696
Galina <galinap684@gmail.com>
2020-05-27 22:33:27 -0400

    add math magic project solution code

---
+++ 2-data-types-variables/Math-Magic/MathMagic.kt
@@ -0,0 +1,19 @@
+fun main() { 
+    
+    // This program determines the user's age using  an arithmetic magic trick. 
+    
+    val userName = "Galina"
+    var magicNum: Int
+
+    magicNum = 2 * 5 
+
+    magicNum += 3 
+
+    magicNum *= 2 
+
+    magicNum += 5
+
+    magicNum -= 6
+    
+    println("$userName's age is $magicNum") 
+}
