324bb813917f79f93a67d562639bc57762bfacd6
Patrick McCorkle <47366424+GasalBanglia@users.noreply.github.com>
2020-07-10 08:34:16 -0400

    Create gasal_banglia_art.kt

---
+++ 1-introduction-to-kotlin/gasal_banglia_art.kt
@@ -0,0 +1,12 @@
+// A robot on a starry night, with a strange looking tree.
+
+/*
+     _^^_
+   <(o__o)>
+ >==( || )==<
+    _//\\_  
+*/
+
+fun main() {
+  println("  *  _^^_     *   (---)    \n * <(o__o)>    *  \\| |  \n >==( || )==<      | |/  \n_____//\\\\_________/___\\ ")
+}
