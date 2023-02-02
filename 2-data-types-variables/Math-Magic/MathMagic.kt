e7130fa7b2c8addca41145fe21209b0021f0db53
Kwizera Claude <71407602+kclaudeeager@users.noreply.github.com>
2023-02-02 16:07:19 +0200

    Create MathMagic.kt

--- 2-data-types-variables/Math-Magic/MathMagic.kt
+++ 2-data-types-variables/Math-Magic/MathMagic.kt
@@ -1,19 +1,10 @@
-fun main() { 
-    
-    // This program determines the user's age using  an arithmetic magic trick. 
-    
-    val userName = "Galina"
-    var magicNum: Int
-
-    magicNum = 2 * 5 
-
-    magicNum += 3 
-
-    magicNum *= 2 
-
-    magicNum += 5
-
-    magicNum -= 6
-    
-    println("$userName's age is $magicNum") 
+fun main() {
+val userName:String="Claude"
+var magicNum:Int
+magicNum=2*5
+magicNum+=3
+magicNum*=2
+magicNum+=6
+magicNum-=6
+println("$userName's age is $magicNum.")
 }
